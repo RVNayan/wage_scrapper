93bab0a379c9338425b0a6942a2b3089fcbd3651
Nayan_rv <127434437+RVNayan@users.noreply.github.com>
2023-06-11 20:51:34 +0530

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -1,51 +1,52 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
 
-chromedriver_path = "/usr/local/bin/chromedriver"
-
+# Set up the Chrome driver
 driver = webdriver.Chrome()
 
 company = input("Please enter the company name: ")
 
-url = f"https://www.levels.fyi/companies/{company}/salaries/software-engineer?yoeChoice=junior&country=113"
+url = f"https://www.levels.fyi/companies/{company}/salaries/software-engineer?yoeChoice=junior"
 
 driver.get(url)
 
-# Click the first button
-button1 = driver.find_element(By.XPATH, '//*[@id="blur-prompt_blurPromptCell__qv6Ha"]/div/div/button')
+button1 = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/div/div[7]/table/tbody/tr[5]/td/div/div/button')
 button1.click()
 
-# Wait for the second button to appear
-time.sleep(2)
 
-# Click the second button
-button2 = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/div/div[8]/table/tbody/tr[5]/td/div/div/button')
+time.sleep(2)
+button2 = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/div/div[7]/table/tbody/tr[5]/td/div/div/button')
 button2.click()
 
-# Wait for the table to load
-time.sleep(4)
+
+time.sleep(2)
 
 # Find the table
-def check(driver):
-    table = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div/div[1]/div/div[8]/table/tbody')
 
 # Find all rows in the table
-    rows = table.find_elements(By.TAG_NAME, "tr")
+
 
 # Iterate over the rows and extract the data
+def check(driver):
+    table = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/div/div[7]/table')
+    time.sleep(5)
+    rows = table.find_elements(By.TAG_NAME, "tr")
     for row in rows:
+        # Find all cells in the row
         cells = row.find_elements(By.TAG_NAME, "td")
 
+        # Extract the text from each cell and print it
         row_data = [cell.text for cell in cells]
         print(row_data)
 
-for i in range(10):
+for i in range(3):
     check(driver)
-    button3 = driver.find_element(By.CSS_SELECTOR, ".MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeSmall.MuiButton-textSizeSmall.MuiButtonBase-root.css-1f3e5dk")
-    button3.click() 
-    time.sleep(5)
-# Clean up and close the browser
+    button3 = driver.find_element(By.XPATH, '//button[contains(@class, "css-1f3e5dk")]')
+
+    button3.click()
+
+
 driver.quit()
 
